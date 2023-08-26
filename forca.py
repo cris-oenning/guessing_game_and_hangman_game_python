7d1a165793217a8bf7401114a38dd47b72b3c502
cris-oenning <crisoenning@gmail.com>
2023-08-26 16:18:49 -0300

    feat: preparando o jogo da forca

--- forca.py
+++ forca.py
@@ -1,9 +1,17 @@
 def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
+  print("*********************************")
+  print("***Bem vindo ao jogo da Forca!***")
+  print("*********************************")
 
-    print("Fim do jogo")
+  palavra_secreta = "banana"
+
+  enforcou = False
+  acertou = False
+
+  while(not enforcou and not acertou):
+    print("jogando ...")
+
+  print("Fim do jogo")
 
 if(__name__ == "__main__"):
-    jogar()
+  jogar()
